77b1ade9b69233c201d1f5306568c625b4ef54d6
Esau Martinez <esaumtz4722@gmail.com>
2026-04-01 11:47:00 -0600

    feat: add analytics and insights data with vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +33,8 @@ export default function RootLayout({
         style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
       >
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
